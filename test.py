f5d592fc59b629adb14ec5b81c569105dd2d6cac
Louis Warren <louis@lsw.nz>
2023-06-23 20:50:13 +1200

    Update tests

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
+import unittest
 import edmv
 import sys
 import io
 
-
-def run_trial(trial):
+def run_trial(trial, retries=0):
     sources = [x[0] for x in trial]
 
     contents = "\n".join((x[1] for x in trial))
@@ -17,77 +17,103 @@ def run_trial(trial):
             s = s[:pos] + '.[UUID].edmv'
         return s
 
+    mv_log = []
     def test_mv(src, dest):
-        src = strip_uuid(src)
-        dest = strip_uuid(dest)
         if 'ERROR' in (src, dest):
-            print(src, "=>", dest, "[simulating error]")
             raise Exception("Simulated error")
         else:
-            print(src, "=>", dest)
+            mv_log.append((strip_uuid(src), strip_uuid(dest)))
 
-    stderr = sys.stderr
+    stdin = sys.stdin
     stdout = sys.stdout
+    stderr = sys.stderr
     try:
+        sys.stdin = io.StringIO('y\n' * retries + 'n\n')
         sys.stdout = io.StringIO()
         sys.stderr = io.StringIO()
-        if edmv.edmv(sources, editor = test_editor, mv = test_mv):
-            print("Pass", file=sys.stderr)
-        else:
-            print("Fail", file=sys.stderr)
-        return sys.stdout.getvalue(), sys.stderr.getvalue()
+        status = edmv.edmv(sources, editor = test_editor, mv = test_mv)
+        return mv_log, status, sys.stdout.getvalue(), sys.stderr.getvalue()
     finally:
         sys.stderr = stderr
         sys.stdout = stdout
+        sys.stdin = stdin
 
-def assert_trial(trial, expected_stdout_lines, expected_stderr=''):
-    out, err = run_trial(trial)
+class TrialTest():
+    def run_trial(self, trial, retries=0):
+        result = run_trial(trial, retries)
+        self.mv_log, self.status, self.stdout, self.stderr = result
 
-    expected_stdout = '\n'.join(expected_stdout_lines)
+    def setUp(self):
+        self.run_trial(self.moves)
 
-    if out.strip() != expected_stdout.strip():
-        raise AssertionError("Expected\n" + expected_stdout + "\nGot\n" + out)
-    if err.strip() != expected_stderr.strip():
-        raise AssertionError("Expected\n" + expected_stderr + "\nGot\n" + err)
+    def testMoves(self):
+        self.assertListEqual(self.mv_log, list(self.expected_moves))
 
-print("Test: normal renaming")
-assert_trial((
+    def testStatus(self):
+        if 'expected_status' in dir(self):
+            self.assertEqual(self.status, self.expected_status)
+        else:
+            self.assertEqual(self.status, True)
+
+    def testStdout(self):
+        if 'expected_stdout' in dir(self):
+            self.assertEqual(self.stdout, self.expected_stdout)
+        else:
+            self.assertEqual(self.stdout, "")
+
+    def testStderr(self):
+        if 'expected_stderr' in dir(self):
+            self.assertEqual(self.stderr, self.expected_stderr)
+        else:
+            self.assertEqual(self.stderr, "")
+
+
+class TestNormal(TrialTest, unittest.TestCase):
+    moves = (
         ('a', 'b'),
-        ('c', 'c')
-), (
-        "a => b",
-), "Pass")
+        ('c', 'c'),
+    )
 
-print("Test: cycling renaming")
-assert_trial((
+    expected_moves = (
         ('a', 'b'),
-        ('b', 'a')
-), (
-        "a => b.[UUID].edmv",
-        "b => a.[UUID].edmv",
-        "b.[UUID].edmv => b",
-        "a.[UUID].edmv => a",
-), "Pass")
+    )
 
-print("Test: mixture")
-assert_trial((
+    expected_stdout = ""
+
+class TestCycling(TrialTest, unittest.TestCase):
+    moves = (
+        ('a', 'b'),
+        ('b', 'a'),
+    )
+
+    expected_moves = (
+        ('a', 'b.[UUID].edmv'),
+        ('b', 'a.[UUID].edmv'),
+        ('b.[UUID].edmv', 'b'),
+        ('a.[UUID].edmv', 'a'),
+    )
+
+class TestMixture(TrialTest, unittest.TestCase):
+    moves = (
         ('a', 'b'),
         ('b', 'c'),
         ('c', 'a'),
         ('1', '1'),
         ('2', '3'),
-), (
-    "a => b.[UUID].edmv",
-    "b => c.[UUID].edmv",
-    "c => a.[UUID].edmv",
-    "2 => 3",
-    "b.[UUID].edmv => b",
-    "c.[UUID].edmv => c",
-    "a.[UUID].edmv => a",
-), "Pass")
+    )
 
-print("Test: fail and undo")
-assert_trial((
+    expected_moves = (
+        ('a', 'b.[UUID].edmv'),
+        ('b', 'c.[UUID].edmv'),
+        ('c', 'a.[UUID].edmv'),
+        ('2', '3'),
+        ('b.[UUID].edmv', 'b'),
+        ('c.[UUID].edmv', 'c'),
+        ('a.[UUID].edmv', 'a'),
+    )
+
+class TestFailAndUndo(TrialTest, unittest.TestCase):
+    moves = (
         ('a', 'b'),
         ('b', 'c'),
         ('c', 'a'),
@@ -95,14 +121,25 @@ assert_trial((
         ('2', '3'),
         ('5', 'ERROR'),
         ('6', '7'),
-), (
-        "a => b.[UUID].edmv",
-        "b => c.[UUID].edmv",
-        "c => a.[UUID].edmv",
-        "2 => 3",
-        "5 => ERROR [simulating error]",
-        "3 => 2",
-        "a.[UUID].edmv => c",
-        "c.[UUID].edmv => b",
-        "b.[UUID].edmv => a",
-), "Error: Simulated error\nUndoing...\nFail")
+    )
+
+    expected_moves = (
+        ('a', 'b.[UUID].edmv'),
+        ('b', 'c.[UUID].edmv'),
+        ('c', 'a.[UUID].edmv'),
+        ('2', '3'),
+        ('3', '2'),
+        ('a.[UUID].edmv', 'c'),
+        ('c.[UUID].edmv', 'b'),
+        ('b.[UUID].edmv', 'a'),
+    )
+
+    expected_stderr = (
+        "Error: Simulated error\n"
+        "Undoing...\n"
+    )
+
+    expected_status = False
+
+if __name__ == '__main__':
+    unittest.main()
